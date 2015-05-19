d973c8634bf4dd04a5309f1374878be00347230b
hopelesscoder <dadda-1993@hotmail.it>
2015-05-19 19:00:19 +0200

    Messaggio per la commit

---
+++ fw/root/set_firewall.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+
+
+
+
+
+
+# Stop all pending rule expiry timers
+killall sleep2 >& /dev/null
+
+# Clear existing iptables rules from relevant chains
+iptables --table nat --flush PREROUTING
+iptables --table nat --flush POSTROUTING
+/etc/init.d/ulogd stop >/dev/null
+
+
+
+      # Appear to the outside with fixed addresses
+	iptables -A INPUT --in-interface eth1 -p tcp --dport 22 -j DROP
+
+
+	#iptables --table nat -A POSTROUTING --jump MASQUERADE --out-interface eth1 --source 192.0.0.0/24 #--destination 0/0
+	#iptables --table nat -A POSTROUTING --jump MASQUERADE --out-interface eth1 --source 150.0.0.0/24 #--destination 0/0
+	#iptables --table nat -A POSTROUTING --jump MASQUERADE --out-interface eth0 --source 192.0.0.0/24 #--destination 0/0
+	#iptables --table nat -A POSTROUTING --jump MASQUERADE --out-interface eth0 --source 150.0.0.0/24 #--destination 0/0
+	#iptables --table nat --append PREROUTING --in-interface eth1 --destination 120.0.0.10  --jump DNAT --to-destination  10.0.0.2
+	
+
+
+
+
+ 
+	#iptables --table nat -A POSTROUTING --out-interface eth0 --jump SNAT --to 10.0.0.2
