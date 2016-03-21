506ae01b48aa3ee0c271a51f3a2c5639bc0a2435
hopelesscoder <dadda-1993@hotmail.it>
2016-03-21 01:25:07 +0100

    Fixato problema su eth0 del nat

--- r-it/root/set_nat_type.sh
+++ r-it/root/set_nat_type.sh
@@ -19,8 +19,8 @@ iptables --table nat --flush POSTROUTING
       # Appear to the outside with fixed addresses
 	iptables --table nat -A POSTROUTING --jump MASQUERADE --out-interface eth1 --source 192.0.0.0/24 #--destination 0/0
 	iptables --table nat -A POSTROUTING --jump MASQUERADE --out-interface eth1 --source 150.0.0.0/24 #--destination 0/0
-	iptables --table nat -A POSTROUTING --jump MASQUERADE --out-interface eth0 --source 192.0.0.0/24 #--destination 0/0
-	iptables --table nat -A POSTROUTING --jump MASQUERADE --out-interface eth0 --source 150.0.0.0/24 #--destination 0/0
+	#iptables --table nat -A POSTROUTING --jump MASQUERADE --out-interface eth0 --source 192.0.0.0/24 #--destination 0/0
+	#iptables --table nat -A POSTROUTING --jump MASQUERADE --out-interface eth0 --source 150.0.0.0/24 #--destination 0/0
 	iptables --table nat --append PREROUTING --in-interface eth1 --destination 120.0.0.10  --jump DNAT --to-destination  10.0.0.2
 	
 
